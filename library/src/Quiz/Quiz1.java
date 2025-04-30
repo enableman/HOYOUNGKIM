977707dfa87beaf7e81d81f5e2024dd596d2f11d
HYK <HYK@DESKTOP-533BRDM>
2025-04-30 10:29:17 +0900

    자바 프로그램 수정

--- library/src/Quiz/Quiz1.java
+++ library/src/Quiz/Quiz1.java
@@ -1,19 +1,34 @@
 package Quiz;
 
 import java.io.File;
+import java.util.Scanner;
 
 import net.coobird.thumbnailator.Thumbnails;
 
 public class Quiz1 {
 
 	public static void main(String[] args) throws Exception{
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("크기를 줄일 파일명을 입력해 주세요.");
+		String str1 = scan.nextLine();
+		
+		System.out.println("변환할 크기의 가로픽셀을 입력해 주세요.");
+		int a = scan.nextInt();
+		
+		System.out.println("변환할 크기의 세로픽셀을 입력해 주세요.");
+		int b = scan.nextInt();
+		
+		System.out.println("변환할 파일명을 입력해 주세요.");
+		String str2 = scan.next();
 		 
 		try {
 			
-			 Thumbnails.of(new File("C:/DEV/aaa.jpg")) //입력파일
-			.size(100,200)                             //파일 크기
+			 Thumbnails.of(new File(str1)) //입력파일
+			.size(a,b)                             //파일 크기
 			.outputFormat("jpg")                       //파일 타입
-			.toFile(new File("aaa_1.jpg"));            //출력파일 
+			.toFile(str2);            //출력파일 
 			 System.out.println("변환이 완료되었습니다.");
 		
 	     } 
@@ -21,6 +36,8 @@ public class Quiz1 {
 			 e.printStackTrace();
 			 System.out.println("오류가 발생되었습니다.");
 	     }
+		
+		scan.close();
 
 	}
 
